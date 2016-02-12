cd02827293917f428f02de52cd3a3e38f326ccf3
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-02-12 16:20:43 +0800

    fix unit test.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- hello-world_test.go
+++ hello-world_test.go
@@ -1,17 +1,48 @@
 package main
 
 import (
-		"net/http"
-		"net/http/httptest"
-		"testing"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
 func TestHello(t *testing.T) {
-		homeHandle := homeHandler()
-		req, _ := http.NewRequest("GET", "", nil)
-		w := httptest.NewRecorder()
-		homeHandle.ServeHTTP(w, req)
-		if w.Code != http.StatusOK {
-				t.Errorf("Home page didn't return %v", http.StatusOK)
-		}
+	type Message struct {
+		Text string
+		Current_Time string
+	}
+
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+	r.Handle("GET", "/", rootHandler)
+	req, err := http.NewRequest("GET", "/", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	// pase json body.
+	dec := json.NewDecoder(strings.NewReader(w.Body.String()))
+	var m Message
+	err2 := dec.Decode(&m)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	if m.Text != "hello world" {
+		t.Errorf("JSON Text value didn't return hello world")
+	}
+
+	if w.Code != http.StatusOK {
+		fmt.Printf("%d - %s", w.Code, w.Body.String())
+		t.Errorf("Home page didn't return %v", http.StatusOK)
+	}
 }
