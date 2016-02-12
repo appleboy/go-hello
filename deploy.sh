adc2ebde5ea3546a763f376682b4035873224a15
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-02-12 10:34:22 +0800

    Using gin framework
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+
+output() {
+  printf "\E[0;33;40m"
+  echo $1
+  printf "\E[0m"
+}
+
+output "Stop container."
 docker stop hello-production
-docker run --rm -p 8000:80 --name hello-production hello-world
+output "Remove container."
+docker rm -f hello-production
+output "Start container."
+docker run -d -p 8000:8000 --name hello-production hello-world
--- hello-world.go
+++ hello-world.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/gin-gonic/gin"
+	"time"
 )
 
-func homeHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+func rootHandler(context *gin.Context) {
+	current_time := time.Now()
+	current_time.Format("20060102150405")
+	context.JSON(200, gin.H{
+		"current_time": current_time,
 	})
 }
 
 func main() {
-	http.Handle("/", homeHandler())
-	http.ListenAndServe(":8088", nil)
+	router := gin.Default()
+	router.GET("/", rootHandler)
+	router.Run(":8000")
 }
